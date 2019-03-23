36440db49c817a4bbc4ddd6cbf6e772358787221
Gabester0 <gceipper@gmail.com>
2019-03-23 13:36:21 -0400

    now deployment setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const debug = process.env.NODE_ENV !== 'production';
-const assetPrefix = debug ? '' : 'https://github.com/Gabester0/chainsaw-markdown';
-
 module.exports = {
-  assetPrefix
-};
+  target: 'serverless'
+}
